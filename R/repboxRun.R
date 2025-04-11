b29508911403bd02d92c11c17d2c7c2ab17aceda
skranz-auto <xsx50@uni-ulm.de>
2025-04-11 09:35:08 +0000

    Update from server

--- R/repboxRun.R
+++ R/repboxRun.R
@@ -5,23 +5,22 @@ example = function() {
   #artid = "ecta_84_2_6"
   #rstudioapi::filesPaneNavigate(paste0("~/repbox/projects_gha/",artid))
 
-  artid = "jeea_12_6_2"
+  artid = "aeri_1_2_6"
   projects.dir = "~/repbox/projects_test"
-  #projects.dir = "~/repbox/projects_gha"
 
   #artid = "testsupp"
-  #repbox_init_ejd_project(artid=artid, projects.dir=projects.dir)
+  repbox_init_ejd_project(artid=artid, projects.dir=projects.dir)
 
   Project_dir = file.path(projects.dir,artid)
   steps = repbox_steps_from(file_info = TRUE)
   #steps = repbox_steps_from(reproduction = TRUE, map=TRUE, html = TRUE)
   #steps = repbox_steps_from(mr_base = TRUE)
-  steps = repbox_steps_from(html=TRUE)
+  #steps = repbox_steps_from(html=TRUE)
   html_opts = repbox_html_opts(make_what = c("ejd","general"))
   #stata_opts = repbox_stata_opts(timeout = 5*60, rerun.failed.included.do = FALSE)
   #opts = repbox_run_opts(stop.on.error = FALSE,html_opts=html_opts)
   #steps = repbox_steps_from(map = TRUE,html=FALSE)
-  steps = repbox_steps_from(file_info = TRUE,art = TRUE,reproduction = FALSE)
+  #steps = repbox_steps_from(file_info = TRUE,art = TRUE,reproduction = FALSE)
   options(warn=1)
   opts = repbox_run_opts(stop.on.error = !TRUE,timeout = 10*60, art_opts = repbox_art_opts(overwrite=TRUE), html_opts = html_opts)
   repbox_run_project(Project_dir,lang="stata", steps=steps, opts=opts)
