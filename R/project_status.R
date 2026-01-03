e29ea2796ca94cd16d5096f83c84f2a025fa3a38
skranz-auto <xsx50@uni-ulm.de>
2026-01-03 17:43:15 +0000

    Update from server

--- R/project_status.R
+++ R/project_status.R
@@ -12,21 +12,24 @@ repbox_project_status = function(project_dir) {
   # the complete status shall consist of different states
   state_li = vector("list", 100)
   state_ind = 0
+  project_dir = normalizePath(project_dir)
 
+  fp = function(file) {
+    file = normalizePath(file,mustWork = FALSE)
+    ifelse(startsWith(file, project_dir), file, paste0(project_dir, "/", file))
+  }
   has_file = function(...) {
-    file.exists(file.path(project_dir, ...))
+    file.exists(fp(...))
   }
-  file_date = function(...) {
-    file.mtime(file.path(project_dir, ...))
-  }
-  add_state = function(stateid,has=if (!is.na(val)) TRUE else NA, timestamp = NA_POSIXct_, help="", val=NA_real_, ...) {
-    state = data.frame(stateid,has, timestamp,val, help, ...)
+  add_state = function(stateid,has=if (!is.na(val)) TRUE else NA, timestamp = NA_POSIXct_,  val=NA_real_, mb=NA_real_, help="", ...) {
+    state = data.frame(stateid,has, timestamp,val,mb, help, ...)
     state_ind <<- state_ind +1
     state_li[[state_ind]] <<- state
     state
   }
   add_file_state = function(stateid, file, ...) {
-    add_state(stateid, has = has_file(file), timestamp=file_date(file), ...)
+    file = fp(file)
+    add_state(stateid, has = file.exists(file), timestamp=file.mtime(file),mb = file.size(file) / 1e6, ...)
   }
   add_dir_state = function(stateid, dir, first=FALSE,...) {
     restore.point("add_dir_state")
@@ -58,7 +61,10 @@ repbox_project_status = function(project_dir) {
   doc_types = repboxDoc::rdoc_type(doc_dirs)
   doc_forms = repboxDoc::rdoc_form(doc_dirs)
 
-  add_state("doc_art",has=any(doc_types != "art"))
+  has_doc_app = length(app_prefix)>0
+
+
+  add_state("doc_art",has=any(doc_types == "art"))
   add_state("doc_app",has=any(doc_types != "art"))
   add_dir_state("doc_art_pdf",doc_dirs[doc_types == "art" & doc_forms=="pdf"])
   add_dir_state("doc_art_mocr",doc_dirs[doc_types == "art" & doc_forms=="mocr"])
@@ -78,15 +84,15 @@ repbox_project_status = function(project_dir) {
 
     r_runtime = stata_runtime = NA_real_
     if (has_file("metareg/base/runtimes.Rds")) {
-      rt = readRDS(file.path(project_dir,"metareg/base/runtimes.Rds"))
+      rt = readRDS(fp("metareg/base/runtimes.Rds"))
       r_runtime = rt$total$r_runtime
       stata_runtime = rt$total$stata_runtime
     }
     add_state("mr_base_r_runtime", val=r_runtime)
     add_state("mr_base_stata_runtime", val=stata_runtime)
 
-    num_infeasible_steps = length(list.files(file.path(project_dir, "metareg/base/step_results"), glob2rx("infeasible*.Rds")))
-    num_reg_results = length(list.files(file.path(project_dir, "metareg/base/step_results"), glob2rx("infeasible*.Rds")))
+    num_infeasible_steps = length(list.files(fp("metareg/base/step_results"), glob2rx("infeasible*.Rds")))
+    num_reg_results = length(list.files(fp("metareg/base/step_results"), glob2rx("infeasible*.Rds")))
 
     add_state("mr_base_num_infeasible_steps", val=num_infeasible_steps)
     add_state("mr_base_num_reg_results", val=num_reg_results)
@@ -96,12 +102,12 @@ repbox_project_status = function(project_dir) {
 
   # readme
   add_state("readme_dir",has=has_file("readme"))
-  add_file_state("readme_ranks",has=has_file("readme/readme_ranks"))
+  add_file_state("readme_ranks","readme/readme_ranks.Rds")
   add_dir_state("readme_txt","readme/txt")
 
   # gha
   if (has_file("gha/gha_status.txt")) {
-    add_state("gha", has = TRUE, timestamp = file_date("gha/gha_status.txt"), status=merge.lines(readLines(file.path(project_dir, "gha/gha_status.txt"))),help="Was run on Github Actions")
+    add_state("gha", has = TRUE, timestamp = file.mtime(fp("gha/gha_status.txt")), status=merge.lines(readLines(fp("gha/gha_status.txt"))),help="Was run on Github Actions")
   } else {
     add_state("gha", has = FALSE, help="Was not run on Github Actions")
   }
@@ -110,10 +116,47 @@ repbox_project_status = function(project_dir) {
   add_file_state("report_do_tab","reports/do_tab.html")
 
 
+  # fp
+  types = "art"
+  if (has_doc_app) type = c(types, "app")
+
+  for (type in types) {
+    library(FuzzyProduction)
+    app_types = setdiff(unique(doc_types),"art")
+
+    type = "art"
+    doc_type = type
+    if (type=="app") doc_type = app_types
+    fp_dir =paste0(fp("fp/prod_"),doc_type)
+    ver_dirs = FuzzyProduction::fp_all_ok_ver_dirs(fp_dir)
+    fp_df = FuzzyProduction::fp_ver_dir_to_ids(ver_dirs)
+
+    fp_df$timestamp = file.mtime(file.path(fp_df$ver_dir, "prod_df.Rds"))
+    p_df = fp_df %>%
+      group_by(prod_id) %>%
+      summarize(
+        timestamp = max(timestamp, na.rm=TRUE),
+        val = n()
+      )
+    if (NROW(p_df)>0) {
+      add_state(paste0("fp_prod_", p_df$prod_id),has=TRUE, timestamp = p_df$timestamp, val=p_df$val, help="val: no. versions")
+    }
+    p_df = fp_df %>%
+      group_by(prod_id, proc_id) %>%
+      summarize(
+        timestamp = max(timestamp, na.rm=TRUE),
+        val = n()
+      )
+    if (NROW(p_df)>0) {
+      add_state(paste0("fp_proc_",p_df$prod_id,"-", p_df$proc_id),has=TRUE, timestamp = p_df$timestamp, val=p_df$val, help="val: no. versions")
+    }
+  }
 
 
-
-
+  repdb_files = list.files(fp("repdb"),glob2rx("*.Rds"), full.names = TRUE)
+  timestamp = file.mtime(repdb_files)
+  parcel = basename(repdb_files) %>% tools::file_path_sans_ext()
+  add_state(paste0("repdb_", parcel), has=TRUE, timestamp=timestamp, mb = file.size(repdb_files) / 1e6)
 
 
   status = bind_rows(state_li)
