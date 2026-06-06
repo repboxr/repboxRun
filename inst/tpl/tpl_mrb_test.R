c98ba2ae8772b10484e4713d282ab7162de552e4
skranz-auto <xsx50@uni-ulm.de>
2026-06-06 07:36:44 +0000

    update

--- inst/tpl/tpl_mrb_test.R
+++ inst/tpl/tpl_mrb_test.R
@@ -11,27 +11,32 @@ if (FALSE) {
   rstudioapi::navigateToFile(file.path(project_dir, "test_report/test_report.Rmd"))
   rstudioapi::filesPaneNavigate(project_dir)
 
+
   # Extra debugging info for a given runid (if report stopped due to error)
-  if (FALSE)
-    mrb_runid_test_files(project_dir, runid=127)
+  # mrb_create_cache_at_runid(project_dir=project_dir, cache_runid=128)
+  # drf = drf_create(project_dir)
+  mrb_pid_test_files(project_dir, pid=128)
+  mrb_test_data_path(project_dir, pid=128)
+
 }
 
 repboxRun::rb_remove_project_dirs(project_dir, clear_all=TRUE)
 rb = rb_new(project_dir)
-if (!FALSE) {
-  rb = rb_update_file_info_parcel(rb, overwrite=!FALSE,assume_org_complete = TRUE)
-  rb = rb_update_script_parcels(rb, overwrite=!FALSE)
-  rb = rb_update_static_code_analysis(rb, overwrite = !FALSE)
+if (TRUE) {
+  rb = rb_update_file_info_parcel(rb, overwrite=TRUE,assume_org_complete = TRUE)
+  rb = rb_update_script_parcels(rb, overwrite=TRUE)
+  rb = rb_update_static_code_analysis(rb, overwrite = TRUE)
 
   rb = rb_create_mod_dir(rb)
-  rb = rb_run_stata_reproduction(rb, overwrite=!FALSE)
+  rb = rb_run_stata_reproduction_raw(rb, overwrite= TRUE)
+  rb = rb_postprocess_stata_reproduction(rb, overwrite= TRUE)
 }
 
 
 drf = repboxDRF::drf_load(project_dir)
 drf_clear_mcache()
 mrb = mrb_init(project_dir,with_try = TRUE)
-if (!FALSE) {
+if (TRUE) {
   options(warn=2)
   mrb = mrb_full_stata_script(mrb)
   mrb = mrb_run_stata_script(mrb)
